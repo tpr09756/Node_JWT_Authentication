a46d7e82d4b44d16df94e7b3075096fac1f063d4
Tiago Rodrigues <tpr09756@gmail.com>
2022-12-28 18:19:27 -0100

    jwt token tested with postman

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { json } = require("express");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 
@@ -8,6 +9,43 @@ app.get("/api", (req, res) => {
     message: "Hey there! Welcome to this API service",
   });
 });
+
+app.post("/api/posts", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: "post created...",
+        authData,
+      });
+    }
+  });
+});
+
+app.post("/api/login", (req, res) => {
+  user = {
+    id: 1,
+    username: "tiago",
+    email: "tiago@gmail.com",
+  };
+
+  jwt.sign({ user: user }, "secretkey", (err, token) => {
+    res.json({
+      token,
+    });
+  });
+});
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (bearerHeader !== undefined) {
+    bearerToken = bearerHeader.split(" ")[1];
+    req.token = bearerHeader;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 
 app.listen(3000, (req, res) => {
   console.log("server started on port 3000");
